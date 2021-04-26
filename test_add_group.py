c4c1c9e8e41b3bb14056a8e83348cb3f818b6b20
polikashinaolya <72521921+polikashinaolya@users.noreply.github.com>
2021-04-26 22:04:53 +0300

    1) создала файл Group.py с классом Group содержащим необходимые параметры 2) импортировала class Group в тест 3) создала тест с добавлением пустой группы

--- test_add_group.py
+++ test_add_group.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
-
-
-def is_alert_present(self):
-    try:
-        self.driver.switch_to_alert()
-    except NoAlertPresentException as e:
-        return False
-    return True
+from Group import Group
 
 
 class Add_Group(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(50)
 
     def test_add_resr_group(self):
         driver = self.driver
         self.open_homepage(driver)
-        self.login(driver)
+        self.login(driver, username="admin", password="secret")
         self.open_groups_page(driver)
-        self.create_group(driver)
+        self.create_group(driver, Group(name='test', header="testtest", footer="test1"))
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def test_add_empty_group(self):
+        driver = self.driver
+        self.open_homepage(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name='', header="", footer=""))
         self.return_to_groups_page(driver)
         self.logout(driver)
 
@@ -33,44 +33,37 @@ class Add_Group(unittest.TestCase):
     def return_to_groups_page(self, driver):
         driver.find_element_by_link_text("group page").click()
 
-    def create_group(self, driver):
+    def create_group(self, driver, group):
         # init new group
         driver.find_element_by_name("new").click()
         # fill form
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("test")
+        driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("testtest")
+        driver.find_element_by_name("group_header").send_keys(group.header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("test1")
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
         # submit create group
         driver.find_element_by_name("submit").click()
 
     def open_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").click()
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_homepage(self, driver):
         driver.get("http://localhost/addressbook/")
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-    
     def tearDown(self):
         self.driver.quit()
 
