4724970019bd239aee73ccad3e1195268f100480
polikashinaolya <72521921+polikashinaolya@users.noreply.github.com>
2021-05-04 16:38:15 +0300

    создана фикстура для тестов

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,139 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
+
 from contact import Contact
+import pytest
 from time import sleep
+from application import Application
 
 
-class AddContact(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_contact(self):
-        self.login(username="admin", password="secret")
-        self.add_contact(Contact(firstname="Olya", middlename= "asddas", lastname="fsdas", nickname="bnbdas",
-                                                       title="asdcvas", company="asdzxas", address="asqwdas", phone_home="123",
-                                                       phone_mobile="234", phone_work="345", fax="456", email="q@mail.ru",
-                                                       email2="s@", email3="3@", homepage="aerwe", address2="assdfsdfs",
-                                                       phone2="asdsdfdfas", notes="asqwqdas", bday="1", bmonth="January",
-                                                        byear="1001", aday="2", amonth="February", ayear="2002"))
-        self.logout()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.add_contact(Contact(firstname="Olya", middlename= "asddas", lastname="fsdas", nickname="bnbdas",
+                                                   title="asdcvas", company="asdzxas", address="asqwdas", phone_home="123",
+                                                   phone_mobile="234", phone_work="345", fax="456", email="q@mail.ru",
+                                                   email2="s@", email3="3@", homepage="aerwe", address2="assdfsdfs",
+                                                   phone2="asdsdfdfas", notes="asqwqdas", bday="1", bmonth="January",
+                                                    byear="1001", aday="2", amonth="February", ayear="2002"))
+    app.logout()
 
-    def open_homepage(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        driver = self.driver
-        self.open_homepage()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        self.open_create_contact_page()
-
-    def open_create_contact_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("add new").click()
-
-    def add_contact(self, contact):
-        driver = self.driver
-        #добавляем строковые поля
-        driver.find_element_by_name("firstname").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(contact.firstname)
-        driver.find_element_by_name("middlename").click()
-        driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys(contact.middlename)
-        driver.find_element_by_name("lastname").click()
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(contact.lastname)
-        driver.find_element_by_name("nickname").click()
-        driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys(contact.nickname)
-        driver.find_element_by_name("title").click()
-        driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys(contact.title)
-        driver.find_element_by_name("company").click()
-        driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys(contact.company)
-        driver.find_element_by_name("address").click()
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys(contact.address)
-        driver.find_element_by_name("home").click()
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys(contact.phone_home)
-        driver.find_element_by_name("mobile").click()
-        driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys(contact.phone_mobile)
-        driver.find_element_by_name("work").click()
-        driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys(contact.phone_work)
-        driver.find_element_by_name("fax").click()
-        driver.find_element_by_name("fax").clear()
-        driver.find_element_by_name("fax").send_keys(contact.fax)
-        driver.find_element_by_name("email").click()
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys(contact.email)
-        driver.find_element_by_name("email2").click()
-        driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys(contact.email2)
-        driver.find_element_by_name("email3").click()
-        driver.find_element_by_name("email3").clear()
-        driver.find_element_by_name("email3").send_keys(contact.email3)
-        driver.find_element_by_name("homepage").click()
-        driver.find_element_by_name("homepage").clear()
-        driver.find_element_by_name("homepage").send_keys(contact.homepage)
-        driver.find_element_by_name("address2").click()
-        driver.find_element_by_name("address2").clear()
-        driver.find_element_by_name("address2").send_keys(contact.address2)
-        driver.find_element_by_name("phone2").click()
-        driver.find_element_by_name("phone2").clear()
-        driver.find_element_by_name("phone2").send_keys(contact.phone2)
-        driver.find_element_by_name("notes").click()
-        driver.find_element_by_name("notes").clear()
-        driver.find_element_by_name("notes").send_keys(contact.notes)
-        #добавляем даты
-        driver.find_element_by_name("bday").click()
-        Select(driver.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        driver.find_element_by_xpath("//option[@value='1']").click()
-        driver.find_element_by_name("bmonth").click()
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        driver.find_element_by_xpath("//option[@value='January']").click()
-        driver.find_element_by_name("byear").click()
-        driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys(contact.byear)
-        driver.find_element_by_name("aday").click()
-        Select(driver.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        driver.find_element_by_xpath("(//option[@value='2'])[2]").click()
-        driver.find_element_by_name("amonth").click()
-        Select(driver.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        driver.find_element_by_xpath("(//option[@value='February'])[2]").click()
-        driver.find_element_by_name("ayear").click()
-        driver.find_element_by_name("ayear").clear()
-        driver.find_element_by_name("ayear").send_keys(contact.ayear)
-        self.confirm_add_contact()
-
-    def confirm_add_contact(self):
-        driver = self.driver
-        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def logout(self):
-        driver = self.driver
-        #return home page
-        driver.find_element_by_link_text("home page").click()
-        sleep(5)
-        #logout
-        driver.find_element_by_link_text("Logout").click()
-
-    def tearDown(self):
-        self.driver.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
--- test_add_group.py
+++ test_add_group.py
@@ -1,73 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from group import Group
+from application import Application
 
 
-class AddGroup(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(50)
-
-    def test_add_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name='test', header="testtest", footer="test1"))
-        self.logout()
-
-    def test_add_empty_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name='', header="", footer=""))
-        self.logout()
-
-    def logout(self):
-        driver = self.driver
-        driver.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        driver = self.driver
-        # init new group
-        driver.find_element_by_name("new").click()
-        # fill form
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit create group
-        driver.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        driver = self.driver
-        self.open_homepage()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        self.open_groups_page()
-
-    def open_homepage(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.driver.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name='test', header="testtest", footer="test1"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name='', header="", footer=""))
+    app.logout()
+
