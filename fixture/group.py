90278055df104b6f84e1143acc81b88f02e6e32f
polikashinaolya <72521921+polikashinaolya@users.noreply.github.com>
2021-05-05 19:45:14 +0300

    тест на редактирование первой группы

--- fixture/group.py
+++ fixture/group.py
@@ -36,3 +36,21 @@ class GroupHelper:
         driver.find_element_by_name("selected[]").click()
         driver.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
+    def edit_first(self, group):
+        driver = self.app.driver
+        self.open_groups_page()
+        driver.find_element_by_name("selected[]").click()
+        driver.find_element_by_name("edit").click()
+        # fill form
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys(group.name)
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(group.header)
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        driver.find_element_by_name("update").click()
+        self.return_to_groups_page()
