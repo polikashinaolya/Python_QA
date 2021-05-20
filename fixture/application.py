f785def60d47795d769bae8096a0e8c5b8f87c94
polikashinaolya <72521921+polikashinaolya@users.noreply.github.com>
2021-05-20 15:22:58 +0300

    повышена интеллектуальность фикстуры: открытие и закрытие браузера защищено от случайного закрытия

--- fixture/application.py
+++ fixture/application.py
@@ -8,10 +8,18 @@ from time import sleep
 class Application:
     def __init__(self):
         self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+
+    def is_valid(self):
+        try:
+            #self.driver.current_url() ПРИ ТАКОМ ВАРИАНТЕ НЕ РАБОТАЕТ ПРОВЕРКА, БРАУЗЕР БРОСАЕТСЯ
+            self.driver.get("http://localhost/addressbook/")
+            return True
+        except:
+            return False
 
     def destroy(self):
         self.driver.quit()
