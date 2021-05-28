c6dceadef5ce1a28efe97f8309c10eefda50b889
polikashinaolya <72521921+polikashinaolya@users.noreply.github.com>
2021-05-28 11:09:46 +0300

    добавлено кэширование и хэширование тестов контактов и групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,17 +14,3 @@ def test_add_group(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-
-
-def test_add_empty_group(app):
-    old_groups = app.group.get_groups_list()
-    group_test = Group(name='test', header="testtest", footer="test1")
-    app.group.create_group(group_test)
-    new_groups = app.group.get_groups_list()
-    #проверяем, что новый список групп стал длиннее
-    assert len(old_groups) + 1 == app.group.count()
-    # проверяем, что списки групп, добавленные программно и через браузер, совпадают
-    old_groups.append(group_test)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
