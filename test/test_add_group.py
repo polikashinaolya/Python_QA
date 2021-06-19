cd95ca299b3a83a924493d501ce98df9ac36ccad
polikashinaolya <72521921+polikashinaolya@users.noreply.github.com>
2021-06-19 12:25:53 +0300

    реализована загрузка тестовых данных из файла

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-import string
-import random
 
 
-def random_strint(prefix, maxlen):
-    symbol = string.ascii_letters + string.digits + ' '*10  #пока убрала string.punctuation чтобы тесты не падали
-    return prefix + ''.join([random.choice(symbol) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name='', header='', footer='')] + [
-    Group(name=random_strint('name', 10), header=random_strint('header', 20), footer=random_strint('footer', 20))
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize("group_test", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group_test):
+def test_add_group(app, json_groups):
+    group_test = json_groups
     old_groups = app.group.get_groups_list()
     app.group.create_group(group_test)
     # проверяем, что новый список групп стал длиннее
